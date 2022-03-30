198b7e1101262abc5e6e35dfa3280dc66fe67bbd
Dmitrii Kamianskii <57534924+dkamianskii@users.noreply.github.com>
2022-03-30 10:36:35 +0300

    added ATR for one point

--- indicators/ATR.py
+++ indicators/ATR.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+from typing import Union
+
+from numpy import double
+
 import indicators.moving_averages as ma
 
 
@@ -25,3 +29,14 @@ def ATR(data: pd.DataFrame, N: int = 14) -> pd.Series:
     atr = ma.SMMA(tr, N)
     atr = pd.Series(data=atr, index=data.index[N:])
     return atr
+
+
+def ATR_one_point(prev_atr: Union[float, double, int],
+                  prev_close: Union[float, double, int],
+                  new_point: pd.Series,
+                  N: int) -> float:
+    high = new_point["High"]
+    low = new_point["Low"]
+
+    tr = np.maximum(high, prev_close) - np.minimum(low, prev_close)
+    return ma.SMMA_one_point(prev_atr, tr, N)

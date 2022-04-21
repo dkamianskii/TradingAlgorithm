38da940203e4ec5e8c6f5fe93731a1c0136d16d1
Dmitrii Kamianskii <57534924+dkamianskii@users.noreply.github.com>
2022-04-21 17:13:22 +0300

    ma_support_levels indicator refactoring

--- indicators/ma_support_levels.py
+++ indicators/ma_support_levels.py
@@ -1,49 +1,117 @@
 import indicators.moving_averages as ma
 import numpy as np
 import pandas as pd
-from typing import Optional, List, Tuple, Dict
-from indicators.abstract_indicator import AbstractIndicator, TradeAction
+from typing import Optional, List, Tuple, Dict, Union
+from indicators.abstract_indicator import AbstractIndicator, TradeAction, TradePointColumn
+from helping.base_enum import BaseEnum
 
-import plotly as py
-import plotly.express as px
 import cufflinks as cf
 import plotly.graph_objects as go
 
 cf.go_offline()
 
 
-class MASupportLevels(AbstractIndicator): # todo реворк индикатора под новый стандарт
+class MAsColumns(BaseEnum):
+    TOP_BORDER = 1,
+    MAIN = 2,
+    BOTTOM_BORDER = 3
+
+
+class MASupportLevels(AbstractIndicator):  # todo реворк индикатора под новый стандарт
+
+    default_ma_periods_for_test = [20, 30, 50, 75, 100, 150, 200]
 
     def __init__(self, data: Optional[pd.DataFrame] = None,
-                 ma_periods: List = None):
+                 ma_periods: Optional[List] = None,
+                 use_tested_MAs: bool = False):
         super().__init__(data)
         self.ma_periods: Optional[List] = None
+        self._use_tested_MAs: bool = use_tested_MAs
         if ma_periods is not None:
             self.set_ma_periods(ma_periods)
         self.tested_MAs: Optional[Dict] = None
-        self.MAs: Optional[Dict[int, pd.DataFrame]] = None
+        self.MAs: Dict[int, pd.DataFrame] = {}
         self.MA_test_results: Optional[List] = None
 
     def set_ma_periods(self, ma_periods: List[int]):
         self.ma_periods = ma_periods
         return self
 
-    default_ma_periods_for_test = [20, 30, 50, 75, 100, 150, 200]
+    def set_tested_MAs_usage(self, use_tested_MAs: bool):
+        self._use_tested_MAs = use_tested_MAs
 
-    def __calculate_MAs(self, ma_periods) -> Dict:
-        MAs = {period: ma.EMA(self.data["Close"], N=period) for period in ma_periods if self.data.shape[0] > period + 1}
-        for period in MAs.keys():
-            m_a = MAs[period]
-            MA_data = {"top border": m_a * 1.002, "main": m_a,
-                       "bottom border": m_a * 0.998}
-            MAs[period] = pd.DataFrame(data=MA_data, index=self.data.index)
-        return MAs
+    def clear_vars(self):
+        pass
+
+    def evaluate_new_point(self, new_point: pd.Series, date: Union[str, pd.Timestamp], special_params: Optional = None):
+        if self._use_tested_MAs:
+            if self.tested_MAs is None:
+                self.test_MAs_for_data()
+        resistance, support = 0, 0
+        activation_flag = False
+        for period, MA in self.MAs.items():
+            new_ma = ma.EMA_one_point(MA[MAsColumns.MAIN][-1], new_point["Close"], period)
+            new_MA_point = {MAsColumns.TOP_BORDER: new_ma * 1.002,
+                            MAsColumns.MAIN: new_ma,
+                            MAsColumns.BOTTOM_BORDER: new_ma * 0.998}
+            is_activation, activation_type = self.__activation_point_check(new_point, new_MA_point)
+            if (not self._use_tested_MAs) or (period in self.tested_MAs.keys()):
+                if is_activation:
+                    activation_flag = True
+                    if activation_type == "resistance":
+                        resistance += 1
+                    else:
+                        support += 1
+            self.MAs[period][date] = new_MA_point
+        self.__make_trade_decision(activation_flag, resistance, support, new_point, date)
+
+    @staticmethod
+    def __activation_point_check(point, MA_point) -> Tuple[bool, Optional[str]]:
+        """
+        Checks if provided data point is activation point for provided MA and support or resistance it is
+
+        :param point: data point with fields "Open", "Close", "High", "Low"
+        :param MA_point: dict with fields MAsColumns.MAIN, MAsColumns.TOP_BORDER, MAsColumns.BOTTOM_BORDER. All fields are float numbers
+        :return: tuple(is point activation:bool, activation type: "support" or "resistance")
+        """
+        if (point["Open"] > MA_point[MAsColumns.MAIN]) and (point["Open"] > point["Close"]):  # support
+            if (point["Close"] <= MA_point[MAsColumns.TOP_BORDER]) or (point["Low"] <= MA_point[MAsColumns.TOP_BORDER]):
+                return True, "support"
+        elif (point["Open"] < MA_point[MAsColumns.MAIN]) and (point["Open"] < point["Close"]):  # resistance
+            if (point["Close"] >= MA_point[MAsColumns.BOTTOM_BORDER]) or (point["High"] >= MA_point[MAsColumns.BOTTOM_BORDER]):
+                return True, "resistance"
+
+        return False, None
+
+    def __make_trade_decision(self, activation_flag: bool, resistance: int, support: int,
+                              new_point: pd.Series, date: Union[str, pd.Timestamp]):
+        if activation_flag:
+            if (resistance == 1) and (support == 0):
+                self.add_trade_point(date, new_point["Close"], TradeAction.SELL)
+            elif (resistance == 0) and (support == 1):
+                self.add_trade_point(date, new_point["Close"], TradeAction.BUY)
+            elif ((resistance == 0) and (support == 0)) or (np.abs(resistance - support) <= 1):
+                self.add_trade_point(date, new_point["Close"], TradeAction.NONE)
+            elif resistance - support == 2:
+                self.add_trade_point(date, new_point["Close"], TradeAction.SELL)
+            elif support - resistance == 2:
+                self.add_trade_point(date, new_point["Close"], TradeAction.BUY)
+            elif resistance - support >= 3:
+                self.add_trade_point(date, new_point["Close"], TradeAction.ACTIVELY_SELL)
+            elif support - resistance >= 3:
+                self.add_trade_point(date, new_point["Close"], TradeAction.ACTIVELY_BUY)
+        self.add_trade_point(date, new_point["Close"], TradeAction.NONE)
 
     def calculate(self, data: Optional[pd.DataFrame] = None):
         super().calculate(data)
         if self.ma_periods is None:
             self.ma_periods = MASupportLevels.default_ma_periods_for_test
-        self.MAs = self.__calculate_MAs(self.ma_periods)
+        for period in self.ma_periods:
+            m_a = ma.EMA(self.data["Close"], N=period)
+            MA_data = {MAsColumns.TOP_BORDER: m_a * 1.002,
+                       MAsColumns.MAIN: m_a,
+                       MAsColumns.BOTTOM_BORDER: m_a * 0.998}
+            self.MAs[period] = pd.DataFrame(data=MA_data, index=self.data.index)
         return self
 
     def test_MAs_for_data(self, days_for_bounce: int = 4) -> List:
@@ -58,14 +126,14 @@ class MASupportLevels(AbstractIndicator): # todo реворк индикатор
         self.MA_test_results = []
         self.tested_MAs = {}
         for period in self.MAs.keys():
-            result = self.__test_MA(self.MAs[period], days_for_bounce)
+            result = self.__test_MA(period, self.MAs[period], days_for_bounce)
             self.MA_test_results.append((period, result))
             if (result["activations"] <= 3) or (result["successes"] / result["activations"] >= 0.45):
                 self.tested_MAs[period] = self.MAs[period]
 
         return self.MA_test_results
 
-    def __test_MA(self, MA: pd.DataFrame, days_for_bounce: int = 4) -> Dict:
+    def __test_MA(self, period: int, MA: pd.DataFrame, days_for_bounce: int) -> Dict:
         """
         Проходим по всем точкам:
         1) Сначала определяем, является ли в данный момент МА линией поддержки или сопротивления.
@@ -78,7 +146,7 @@ class MASupportLevels(AbstractIndicator): # todo реворк индикатор
          Если Close опускается ниже нижней границы МА, то Активация считается неуспешной.
         4) Считается общее количество Активаций и Успехов (для тестирования записываются индексы активации и тип активации в dict)
         """
-        if (days_for_bounce < 2):
+        if days_for_bounce < 2:
             raise ValueError("days for bounce can't be less then 2")
 
         activation_points = {"Dates": [], "activation types": [], "results": []}
@@ -87,10 +155,10 @@ class MASupportLevels(AbstractIndicator): # todo реворк индикатор
         activation_flag: bool = False
         activation_type: Optional[str] = None
         days_counter = 0
-        for date, MA_point in MA.iterrows():
+        for date, MA_point in MA[period:].iterrows():
             point = self.data.loc[date]
             if activation_flag:
-                if days_counter == days_for_bounce:
+                if days_counter > days_for_bounce:
                     activation_points["results"].append("fail")
                     activation_flag = False
                 elif activation_type == "support":
@@ -123,68 +191,22 @@ class MASupportLevels(AbstractIndicator): # todo реворк индикатор
         return {"activations": activations, "successes": successes,
                 "activation_points": pd.DataFrame(activation_points)}
 
-    def __activation_point_check(self, point, MA_point) -> Tuple[bool, Optional[str]]:
-        """
-        Checks if provided data point is activation point for provided MA and support or resistance it is
-
-        :param point: data point with fields "Open", "Close", "High", "Low"
-        :param MA_point: dict with fields "main", "top border", "bottom border". All fields are float numbers
-        :return: tuple(is point activation:bool, activation type: "support" or "resistance")
-        """
-        if (point["Open"] > MA_point["main"]) and (point["Open"] > point["Close"]):  # support
-            if (point["Close"] <= MA_point["top border"]) or (point["Low"] <= MA_point["top border"]):
-                return True, "support"
-        elif (point["Open"] < MA_point["main"]) and (point["Open"] < point["Close"]):  # resistance
-            if (point["Close"] >= MA_point["bottom border"]) or (point["High"] >= MA_point["bottom border"]):
-                return True, "resistance"
-
-        return False, None
-
-    def find_trade_points(self, use_tested_MAs: bool = False) -> pd.DataFrame:
-        if use_tested_MAs:
-            if self.tested_MAs is None:
-                self.test_MAs_for_data()
-            MAs_in_use = self.tested_MAs
-        else:
-            MAs_in_use = self.MAs
-
-        for date, point in self.data[1:].iterrows():
+    def find_trade_points(self) -> pd.DataFrame:
+        intend = np.max(self.ma_periods)
+        for date, point in self.data[intend:].iterrows():
             resistance, support = 0, 0
             activation_flag = False
-            if date == pd.Timestamp(ts_input="2021-01-19"):
-                i = 1
-            for period, MA in MAs_in_use.items():
+            for period, MA in self.MAs.items():
                 MA_point = MA.loc[date]
                 is_activation, activation_type = self.__activation_point_check(point, MA_point)
-                if is_activation:
-                    activation_flag = True
-                    if activation_type == "resistance":
-                        resistance += 1
-                    else:
-                        support += 1
-
-            if activation_flag:
-                if (resistance == 1) and (support == 0):
-                    self.add_trade_point(date, TradeAction.SELL)
-                    continue
-                if (resistance == 0) and (support == 1):
-                    self.add_trade_point(date, TradeAction.BUY)
-                    continue
-                if ((resistance == 0) and (support == 0)) or (np.abs(resistance - support) <= 1):
-                    continue
-                if resistance - support == 2:
-                    self.add_trade_point(date, TradeAction.SELL)
-                    continue
-                if support - resistance == 2:
-                    self.add_trade_point(date, TradeAction.BUY)
-                    continue
-                if resistance - support >= 3:
-                    self.add_trade_point(date, TradeAction.ACTIVELY_SELL)
-                    continue
-                if support - resistance >= 3:
-                    self.add_trade_point(date, TradeAction.ACTIVELY_BUY)
-                    continue
-
+                if (not self._use_tested_MAs) or (period in self.tested_MAs.keys()):
+                    if is_activation:
+                        activation_flag = True
+                        if activation_type == "resistance":
+                            resistance += 1
+                        else:
+                            support += 1
+            self.__make_trade_decision(activation_flag, resistance, support, point, date)
         return self.trade_points
 
     def plot(self, start_date: Optional[pd.Timestamp] = None, end_date: Optional[pd.Timestamp] = None):
@@ -209,10 +231,10 @@ class MASupportLevels(AbstractIndicator): # todo реворк индикатор
         for period, MA in self.MAs.items():
             if ((self.tested_MAs is not None) and (len(self.tested_MAs.keys()) != 0) and (
                     period in self.tested_MAs.keys())):
-                fig.add_trace(go.Scatter(x=MA.index, y=MA["main"], mode='lines',
+                fig.add_trace(go.Scatter(x=MA.index, y=MA[MAsColumns.MAIN], mode='lines',
                                          line=dict(width=2), name=f"{period} EMA tested"))
             else:
-                fig.add_trace(go.Scatter(x=MA.index, y=MA["main"], mode='lines',
+                fig.add_trace(go.Scatter(x=MA.index, y=MA[MAsColumns.MAIN], mode='lines',
                                          line=dict(width=1, dash='dash'), name=f"{period} EMA"))
 
         selected_trade_points = self.select_action_trade_points(start_date=start_date, end_date=end_date)
